171d92f601e6be62421c13953016b0e3de16b989
Felipe Augusto <felipementel@hotmail.com>
2024-11-05 14:12:19 -0300

    feat: update cache

--- src/PoC.AzureCosmosDbNoSQL/8-Cache/Cache.cs
+++ src/PoC.AzureCosmosDbNoSQL/8-Cache/Cache.cs
@@ -3,7 +3,6 @@ using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Cosmos.Fluent;
 using Microsoft.Azure.Cosmos.Serialization.HybridRow.Schemas;
 using Microsoft.Extensions.Configuration;
-using System.ComponentModel;
 using System.Diagnostics;
 
 namespace PoC.AzureCosmosDbNoSQL._7_Performance
@@ -12,13 +11,120 @@ namespace PoC.AzureCosmosDbNoSQL._7_Performance
     {
         public async Task SelectUsingCacheConnection(IConfiguration configuration)
         {
-            string? endpointServerless = configuration.GetSection("CosmosDBForNoSQL:Serverless:Endpoint").Value;
-            string? keyServerless = configuration.GetSection("CosmosDBForNoSQL:Serverless:Key").Value;
+            var containerThrought = await CreateDatabase_Throughput(configuration);
+            var containerCache = await CreateDatabase_Cache(configuration);
 
-            var client = new CosmosClientBuilder(endpointServerless, keyServerless).Build();
+            LoadInfo(containerThrought.Container, containerCache.Container);
+
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            var itemThroughput = containerThrought.Container.GetItemLinqQueryable<Product>(allowSynchronousQueryExecution: true)
+                .Where(p => p.Price > 10)
+                .OrderBy(p => p.Price)
+                .Select(p => new { p.Id, p.Name, p.Price });
+
+            stopwatch.Stop();
+            Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds} ms");
+
+            //Cache
+            stopwatch.Restart();
+
+            var itemCache = containerThrought.Container.GetItemLinqQueryable<Product>(allowSynchronousQueryExecution: true)
+                .Where(p => p.Price > 10)
+                .OrderBy(p => p.Price)
+                .Select(p => new { p.Id, p.Name, p.Price });
+
+            stopwatch.Stop();
+            Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds} ms");
+
+
+
+
+
+
+            ////Nível do item
+            //ItemRequestOptions operationOptions = new()
+            //{
+            //    ConsistencyLevel = ConsistencyLevel.Eventual,
+            //    DedicatedGatewayRequestOptions = new()
+            //    {
+            //        MaxIntegratedCacheStaleness = TimeSpan.FromMinutes(15)
+            //    },
+            //    IfMatchEtag = "etag"
+            //};
+
+            //var item2 = new Product
+            //{
+            //    Id = Guid.NewGuid().ToString(),
+            //    Name = "Product 1",
+            //    Price = 100
+            //};
+
+            //Microsoft.Azure.Cosmos.PartitionKey partitionKey = new("category");
+
+            //await container.UpsertItemAsync<Product>(item2, partitionKey, requestOptions: operationOptions);
+
+
+            ////Nível da Pesquisa
+            //QueryRequestOptions queryOptions = new()
+            //{
+            //    ConsistencyLevel = ConsistencyLevel.Eventual,
+            //    DedicatedGatewayRequestOptions = new()
+            //    {
+            //        MaxIntegratedCacheStaleness = TimeSpan.FromSeconds(120)
+            //    }
+            //};
+
+            //string sql = "SELECT * FROM products p";
+
+            //QueryDefinition query = new(sql);
+
+            //FeedIterator<Product> iterator5 = container.GetItemQueryIterator<Product>(query, requestOptions: queryOptions);
+        }
+
+        private void LoadInfo(
+            Microsoft.Azure.Cosmos.Container containerThroughput,
+            Microsoft.Azure.Cosmos.Container containerCache)
+        {
+            var ProductFaker = new Faker<Product>("pt_BR")
+                .RuleFor(p => p.Id, f => Guid.NewGuid().ToString())
+                .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+                .RuleFor(p => p.CategoryId, f => Guid.NewGuid().ToString())
+                .RuleFor(p => p.Category, f => f.Commerce.Categories(1)[0])
+                .RuleFor(p => p.Price, f => f.Random.Double(1, 100))
+                .RuleFor(p => p.Tags, f => f.Commerce.Categories(3).ToArray())
+                .FinishWith((f, u) =>
+                {
+                    //Console.WriteLine("Product Created Id={0}", u.Id);
+                });
+
+            var productsToInsert = ProductFaker.Generate(300);
+
+            productsToInsert.Select(itemToInsert =>
+                containerThroughput.CreateItemAsync(
+                    itemToInsert,
+                    new Microsoft.Azure.Cosmos.PartitionKey(itemToInsert.Category)));
+
+            productsToInsert.Select(itemToInsert =>
+               containerCache.CreateItemAsync(
+                   itemToInsert,
+                   new Microsoft.Azure.Cosmos.PartitionKey(itemToInsert.Category)));
+        }
+
+        private async Task<ContainerResponse> CreateDatabase_Cache(IConfiguration configuration)
+        {
+            string? endpointCache = configuration
+                .GetSection("CosmosDBForNoSQL:Cache:Endpoint").Value;
+
+            string? keyCache = configuration
+                .GetSection("CosmosDBForNoSQL:Cache:Key").Value;
+
+            var client = new CosmosClientBuilder(endpointCache, keyCache)
+                .Build();
 
             //mode 1 - SERVERLESS
-            client = new(endpointServerless, keyServerless, new CosmosClientOptions()
+            client = new(endpointCache, keyCache, new CosmosClientOptions()
             {
                 //ApplicationRegion = Regions.BrazilSouth,
                 SerializerOptions = new CosmosSerializationOptions()
@@ -32,7 +138,7 @@ namespace PoC.AzureCosmosDbNoSQL._7_Performance
             });
 
             Microsoft.Azure.Cosmos.Database databaseCanalDEPLOY = client.GetDatabase("CanalDEPLOY-Cache");
-            
+
             if (databaseCanalDEPLOY != null)
             {
                 await databaseCanalDEPLOY.DeleteAsync();
@@ -50,75 +156,55 @@ namespace PoC.AzureCosmosDbNoSQL._7_Performance
                 ResolutionPath = "/_ts"
             };
 
-            container = await databaseCanalDEPLOY
+            return await databaseCanalDEPLOY!
                 .CreateContainerIfNotExistsAsync(containerProperties);
+        }
 
-            var ProductFaker = new Faker<Product>("pt_BR")
-                .RuleFor(p => p.Id, f => Guid.NewGuid().ToString())
-                .RuleFor(p => p.Name, f => f.Commerce.ProductName())
-                .RuleFor(p => p.CategoryId, f => Guid.NewGuid().ToString())
-                .RuleFor(p => p.Category, f => f.Commerce.Categories(1)[0])
-                .RuleFor(p => p.Price, f => f.Random.Double(1, 100))
-                .RuleFor(p => p.Tags, f => f.Commerce.Categories(3).ToArray())
-                .FinishWith((f, u) =>
-                {
-                    //Console.WriteLine("Product Created Id={0}", u.Id);
-                });
+        private async Task<ContainerResponse> CreateDatabase_Throughput(IConfiguration configuration)
+        {
+            string? endpointThroughput = configuration
+                .GetSection("CosmosDBForNoSQL:Throughput:Endpoint").Value;
 
-            var product = ProductFaker.Generate(300);
+            string? keyThroughput = configuration
+                .GetSection("CosmosDBForNoSQL:Throughput:Key").Value;
 
+            var client = new CosmosClientBuilder(endpointThroughput, keyThroughput)
+                .Build();
 
-            Stopwatch stopwatch = Stopwatch.StartNew();
-
-            var item = container.GetItemLinqQueryable<Product>(allowSynchronousQueryExecution: true)
-                .Where(p => p.Price > 10)
-                .OrderBy(p => p.Price)
-                .Select(p => new { p.Id, p.Name, p.Price });
-
-            stopwatch.Stop();
-
-            Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds} ms");
-
-            //Cache
-
-            //Nível do item
-            ItemRequestOptions operationOptions = new()
+            client = new(endpointThroughput, keyThroughput, new CosmosClientOptions()
             {
-                ConsistencyLevel = ConsistencyLevel.Eventual,
-                DedicatedGatewayRequestOptions = new()
+                //ApplicationRegion = Regions.BrazilSouth,
+                SerializerOptions = new CosmosSerializationOptions()
                 {
-                    MaxIntegratedCacheStaleness = TimeSpan.FromMinutes(15)
+                    PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase,
+                    IgnoreNullValues = true
                 },
-                IfMatchEtag = "etag"
-            };
+                ConnectionMode = ConnectionMode.Direct, // <-- Direct não utiliza cache
+                ConsistencyLevel = ConsistencyLevel.Session,
+                ApplicationName = "Canal DEPLOY - Azure Cosmos DB NoSQL"
+            });
 
-            var item2 = new Product
+            Microsoft.Azure.Cosmos.Database databaseCanalDEPLOY = client.GetDatabase("CanalDEPLOY-Cache");
+
+            if (databaseCanalDEPLOY != null)
             {
-                Id = Guid.NewGuid().ToString(),
-                Name = "Product 1",
-                Price = 100
-            };
-
-            Microsoft.Azure.Cosmos.PartitionKey partitionKey = new("category");
-
-            await container.UpsertItemAsync<Product>(item2, partitionKey, requestOptions: operationOptions);
+                await databaseCanalDEPLOY.DeleteAsync();
+            }
 
 
-            //Nível da Pesquisa
-            QueryRequestOptions queryOptions = new()
+            Microsoft.Azure.Cosmos.Container container;
+
+            ContainerProperties containerProperties = new("products", "/categoryId");
+            containerProperties.IndexingPolicy.Automatic = true;
+            containerProperties.IndexingPolicy.IndexingMode = IndexingMode.Consistent;
+            containerProperties.ConflictResolutionPolicy = new ConflictResolutionPolicy
             {
-                ConsistencyLevel = ConsistencyLevel.Eventual,
-                DedicatedGatewayRequestOptions = new()
-                {
-                    MaxIntegratedCacheStaleness = TimeSpan.FromSeconds(120)
-                }
+                Mode = ConflictResolutionMode.LastWriterWins,
+                ResolutionPath = "/_ts"
             };
 
-            string sql = "SELECT * FROM products p";
-
-            QueryDefinition query = new(sql);
-
-            FeedIterator<Product> iterator5 = container.GetItemQueryIterator<Product>(query, requestOptions: queryOptions);
-        }   
+            return await databaseCanalDEPLOY!
+                .CreateContainerIfNotExistsAsync(containerProperties);
+        }
     }
 }
